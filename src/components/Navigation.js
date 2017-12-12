c86e773e2c8249daeab145f0783a8e0e0ae545cd
Hunter Chang <hunter@madwiremedia.com>
2017-12-11 19:38:15 -0700

    adding react youtube background post

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,6 +7,7 @@ const Navigation = (props) => (
             <ul className="nav-main__list">
                 <li><Link to="/" onClick={props.handleToggleNav}>Home</Link></li>
                 <li><Link to="/gatsby-starters/" onClick={props.handleToggleNav}>Gatsby.js Starters And Templates</Link></li>
+                <li><Link to="/react-youtube-background/" onClick={props.handleToggleNav}>React YouTube Video Background</Link></li>
                 <li><Link to="/gatsby-featured-images/" onClick={props.handleToggleNav}>Featured Blog Images In Gatsby.js</Link></li>
                 <li><Link to="/using-gatsby-image/" onClick={props.handleToggleNav}>Using Gatsby Image</Link></li>
                 <li><Link to="/wordpress-to-gatsbyjs/" onClick={props.handleToggleNav}>From Wordpress to Gatsby.js</Link></li>
